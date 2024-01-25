8f06c97889576cc3f2fe6cba30316e48753013ee
TeodorK0907 <ted_kehayov@abv.bg>
2024-01-25 21:30:44 +0200

    Added filterOptions for both User and Post model. Added commentDTO and commentMapper. Added commentRepository

---
+++ src/main/java/com/forumsystem/modelhelpers/PostModelFilterOptions.java
@@ -0,0 +1,66 @@
+package com.forumsystem.modelhelpers;
+
+import java.util.Optional;
+
+public class PostModelFilterOptions {
+
+    private Optional<String> title;
+    private Optional<Integer> likes;
+    private Optional<Integer> dislikes;
+    //todo potentially to add tags
+    private Optional<String> sortBy;
+    private Optional<String> sortOrder;
+
+    public PostModelFilterOptions(String title,
+                                  Integer likes,
+                                  Integer dislikes,
+                                  String sortBy,
+                                  String sortOrder) {
+        this.title = Optional.ofNullable(title);
+        this.likes = Optional.ofNullable(likes);
+        this.dislikes = Optional.ofNullable(dislikes);
+        this.sortBy = Optional.ofNullable(sortBy);
+        this.sortOrder = Optional.ofNullable(sortOrder);
+
+    }
+
+    public Optional<String> getSortBy() {
+        return sortBy;
+    }
+
+    public void setSortBy(Optional<String> sortBy) {
+        this.sortBy = sortBy;
+    }
+
+    public Optional<String> getSortOrder() {
+        return sortOrder;
+    }
+
+    public void setSortOrder(Optional<String> sortOrder) {
+        this.sortOrder = sortOrder;
+    }
+
+    public Optional<String> getTitle() {
+        return title;
+    }
+
+    public void setTitle(Optional<String> title) {
+        this.title = title;
+    }
+
+    public Optional<Integer> getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Optional<Integer> likes) {
+        this.likes = likes;
+    }
+
+    public Optional<Integer> getDislikes() {
+        return dislikes;
+    }
+
+    public void setDislikes(Optional<Integer> dislikes) {
+        this.dislikes = dislikes;
+    }
+}
---
+++ src/main/java/com/forumsystem/repositories/CommentRepository.java
@@ -0,0 +1,18 @@
+package com.forumsystem.repositories;
+
+import com.forumsystem.models.Comment;
+import com.forumsystem.models.Post;
+
+import java.util.List;
+
+public interface CommentRepository {
+    List<Comment> getAll();
+
+    Comment getById(int id);
+
+    void create(Comment comment);
+
+    void update(Comment comment);
+
+    void delete(Comment comment);
+}
