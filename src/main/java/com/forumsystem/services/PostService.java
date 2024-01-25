972b901111be7dc6c1a77bdd9287c1f5e37aeb97
TeodorK0907 <ted_kehayov@abv.bg>
2024-01-25 21:29:42 +0200

    Added isArchived field for soft delete and overloaded both equals and hashCode for user model.

--- src/main/java/com/forumsystem/services/PostService.java
+++ src/main/java/com/forumsystem/services/PostService.java
@@ -1,21 +1,27 @@
 package com.forumsystem.services;
 
+import com.forumsystem.modelhelpers.PostModelFilterOptions;
+import com.forumsystem.models.Comment;
 import com.forumsystem.models.Post;
-import com.forumsystem.models.PostDto;
 import com.forumsystem.models.User;
 
 import java.util.List;
 
 public interface PostService {
-    List<Post> getAllPosts(User user);
-    List<Post> getAllPostsCreatedByOtherUsers(User user);
-    List<Post> getPostsForUser(User user);
+    //List<Post> getAllPosts(User user, PostModelFilterOptions postFilter);
+
+    List<Post> getAll(User user, PostModelFilterOptions postFilter);
 
     Post getById(User user, int id);
 
-    Post create(User user, Post post);
+    void create(User user, Post post);
 
-    Post update(User user, Post post);
+    void createComment(User user, Comment comment, int postId);
+
+    Post updatePost(User user, Post post);
+
+    Comment updateComment(User user, Comment comment, int postId);
 
     void delete(User user, int id);
+    void deleteComment(User user, int postId, int commentId);
 }
