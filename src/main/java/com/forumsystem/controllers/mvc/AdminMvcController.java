6e8c4539e3fcc76843780669ef75aa0d919c0320
TeodorK0907 <ted_kehayov@abv.bg>
2024-02-13 12:43:22 +0200

    Added AdminMvcController, Admin views for Dashboard, AllPosts and AllUsers and styling for them.

---
+++ src/main/java/com/forumsystem/controllers/mvc/AdminMvcController.java
@@ -0,0 +1,141 @@
+package com.forumsystem.controllers.mvc;
+
+import com.forumsystem.modelhelpers.AuthenticationHelper;
+import com.forumsystem.modelhelpers.PostModelFilterOptions;
+import com.forumsystem.modelhelpers.UserModelFilterOptions;
+import com.forumsystem.modelmappers.PostResponseMapper;
+import com.forumsystem.models.Post;
+import com.forumsystem.models.User;
+import com.forumsystem.models.modeldto.PostModelFilterDto;
+import com.forumsystem.models.modeldto.UserModelFilterDto;
+import com.forumsystem.repositories.contracts.UserRepository;
+import com.forumsystem.services.contracts.PostService;
+import com.forumsystem.services.contracts.UserService;
+import com.forumsystem.еxceptions.AuthenticationFailureException;
+import com.forumsystem.еxceptions.UnauthorizedOperationException;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+import static com.forumsystem.modelhelpers.ModelConstantHelper.UNAUTHORIZED;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminMvcController {
+
+    private final AuthenticationHelper authHelper;
+    private final UserService userService;
+    private final PostService postService;
+
+    @Autowired
+    public AdminMvcController(AuthenticationHelper authHelper,
+                              UserService userService,
+                              PostService postService) {
+        this.authHelper = authHelper;
+        this.userService = userService;
+        this.postService = postService;
+    }
+
+    @ModelAttribute("isAuthenticated")
+    public boolean populateIsAuthenticated(HttpSession session) {
+        return session.getAttribute("currentUser") != null;
+    }
+
+    @GetMapping()
+    public String showAdminDashboard(HttpSession session, Model model) {
+
+        User user;
+        try {
+            user = authHelper.tryGetUser(session);
+        } catch (AuthenticationFailureException e) {
+            return "redirect:/auth/login";
+        }
+        //User user = userService.getUserByUsername("john_doe");
+
+        try {
+            userService.checkIfAdmin(user);
+            return "AdminPanelView"; // to return a view for access denied.
+        } catch (UnauthorizedOperationException e) {
+            model.addAttribute("statusCode", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+            model.addAttribute("error", UNAUTHORIZED);
+            return "UnauthorizedView"; // to redirect user to login view
+        }
+    }
+
+    @GetMapping("/posts")
+    public String ShowAllPosts(
+            Model model,
+            @ModelAttribute("postFilterOptions") PostModelFilterDto postFilterDto,
+            HttpSession session) {
+
+        User user;
+        try {
+            user = authHelper.tryGetUser(session);
+        } catch (AuthenticationFailureException e) {
+            return "redirect:/auth/login";
+        }
+
+        //User user = userService.getUserByUsername("john_doe");
+        PostModelFilterOptions postFilter = new PostModelFilterOptions(
+                postFilterDto.getTitle(),
+                postFilterDto.getLikes(),
+                postFilterDto.getDislikes(),
+                postFilterDto.getTagName(),
+                postFilterDto.getSortBy(),
+                postFilterDto.getSortOrder());
+
+        try {
+            userService.checkIfAdmin(user);
+            List<Post> posts = postService.getAll(user, postFilter);
+            //List<PostResponseDto> outputPosts = postResponseMapper.convertToDTO(posts);
+            model.addAttribute("postFilterOptions", postFilterDto);
+            model.addAttribute("posts", posts);
+            return "AdminPostsView";
+
+        } catch (UnauthorizedOperationException e) {
+            model.addAttribute("statusCode", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+            model.addAttribute("error", UNAUTHORIZED);
+            return "UnauthorizedView";
+        }
+
+    }
+
+    @GetMapping("/users")
+    public String showAllUsers(Model model,
+                               @ModelAttribute("userFilterOptions") UserModelFilterDto userFilterDto,
+                               HttpSession session) {
+        User user;
+        try {
+            user = authHelper.tryGetUser(session);
+        } catch (AuthenticationFailureException e) {
+            return "redirect:/auth/login";
+        }
+
+        // User user = userService.getUserByUsername("john_doe");
+        UserModelFilterOptions userFilter = new UserModelFilterOptions(
+                userFilterDto.getUsername(),
+                userFilterDto.getEmail(),
+                userFilterDto.getFirstName(),
+                userFilterDto.getSortBy(),
+                userFilterDto.getSortOrder());
+
+        try {
+            userService.checkIfAdmin(user);
+            List<User> users = userService.getAll(user, userFilter);
+            model.addAttribute("userFilterOptions", userFilterDto);
+            model.addAttribute("users", users);
+            return "AdminUsersView";
+        } catch (UnauthorizedOperationException e) {
+            model.addAttribute("statusCode", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+            model.addAttribute("error", UNAUTHORIZED);
+            return "UnauthorizedView";
+        }
+    }
+}
